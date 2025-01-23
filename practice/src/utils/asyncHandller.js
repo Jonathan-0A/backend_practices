ea5fb862222cfb9f275e1e243acb8b6680cbc55b
Jonathan-0A <whiteanonymous123456789@gmail.com>
2025-01-23 04:48:41 -0800

    new endpoints

--- practice/src/utils/asyncHandller.js
+++ practice/src/utils/asyncHandller.js
@@ -1,7 +1,7 @@
-// const asyncHandler = (fn) => {
+// const asyncHandler = (reqHandler) => {
 //     async (req, res, next) => {
 //         try {
-//             await fn(req, res, next);
+//             await reqHandler(req, res, next);
 //         } catch (err) {
 //             res.status(err.code || 500).json({
 //                 success: false,
